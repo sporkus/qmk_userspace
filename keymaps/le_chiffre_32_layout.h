2c6b88ec8757e37df4287eb4374f98cea34840ad
spork <feedthefoon@gmail.com>
2023-06-29 16:16:03 -0700

    bootmagic key and small tweaks

--- keymaps/le_chiffre_32_layout.h
+++ keymaps/le_chiffre_32_layout.h
@@ -5,7 +5,7 @@
 
 // This file should not be called [keyboard].h as it will cause conflict with qmk
 // layout to be used in [keyboard].json | exact layout name defined in keyboards/[keyboard]/info.json
-#define LAYOUT_knob_w(...)        LAYOUT_knob(__VA_ARGS__)
+#define LAYOUT_w(...)        LAYOUT(__VA_ARGS__)
 
 // layers
 #define BASE_LAYER \
@@ -15,21 +15,21 @@
         LEFT_THUMB(_BASE),                               RIGHT_THUMB(_BASE)
 
 #define NUM_LAYER \
-        LEFT_ROW1(_NUM),      _______,     RIGHT_ROW1(_NUM),    \
-        LEFT_ROW2(_NUM),                   RIGHT_ROW2(_NUM),    \
-        LEFT_ROW3(_NUM),                   RIGHT_ROW3(_NUM),    \
-        LEFT_THUMB(_NUM),                  RIGHT_THUMB(_NUM)
+        LEFT_ROW1(_NUM),            _______,             RIGHT_ROW1(_NUM),    \
+        LEFT_ROW2(_NUM),                                 RIGHT_ROW2(_NUM),    \
+        LEFT_ROW3(_NUM),                                 RIGHT_ROW3(_NUM),    \
+        LEFT_THUMB(_NUM),                                RIGHT_THUMB(_NUM)
 
 
 #define NAV_LAYER \
-        LEFT_ROW1(_NAV),      _______,     RIGHT_ROW1(_NAV),    \
-        LEFT_ROW2(_NAV),                   RIGHT_ROW2(_NAV),    \
-        LEFT_ROW3(_NAV),                   RIGHT_ROW3(_NAV),    \
-        LEFT_THUMB(_NAV),                  RIGHT_THUMB(_NAV)
+        LEFT_ROW1(_NAV),            _______,             RIGHT_ROW1(_NAV),    \
+        LEFT_ROW2(_NAV),                                 RIGHT_ROW2(_NAV),    \
+        LEFT_ROW3(_NAV),                                 RIGHT_ROW3(_NAV),    \
+        LEFT_THUMB(_NAV),                                RIGHT_THUMB(_NAV)
 
 
 #define FN_LAYER \
-        LEFT_ROW1(_FN),      _______,     RIGHT_ROW1(_FN),    \
-        LEFT_ROW2(_FN),                   RIGHT_ROW2(_FN),    \
-        LEFT_ROW3(_FN),                   RIGHT_ROW3(_FN),    \
-        LEFT_THUMB(_FN),                  RIGHT_THUMB(_FN)
+        LEFT_ROW1(_FN),             _______,             RIGHT_ROW1(_FN),    \
+        LEFT_ROW2(_FN),                                  RIGHT_ROW2(_FN),    \
+        LEFT_ROW3(_FN),                                  RIGHT_ROW3(_FN),    \
+        LEFT_THUMB(_FN),                                 RIGHT_THUMB(_FN)
