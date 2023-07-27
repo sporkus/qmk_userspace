708fb582a5846a5a48579615acdfa680f266e70f
spork <feedthefoon@gmail.com>
2023-07-26 21:19:00 -0700

    flip vol direction

--- keyboards/sporkus/le_chiffre_32/keymaps/vial/keymap.c
+++ keyboards/sporkus/le_chiffre_32/keymaps/vial/keymap.c
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 #undef BLANK_LAYER
 #define BLANK_LAYER { ENCODER_CCW_CW(_______, _______) }
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
-    [0] =   { ENCODER_CCW_CW(KC_VOLU, KC_VOLD) },
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
     [1] =   BLANK_LAYER,
     [2] =   BLANK_LAYER
 };
