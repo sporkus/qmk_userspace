0c9f6e6d823777985424dd7c801e8993c0c0de7c
spork <feedthefoon@gmail.com>
2023-11-09 16:43:58 -0800

    capsword combos

--- config.h
+++ config.h
@@ -1,6 +1,7 @@
 #pragma once
 
-#define CAPS_WORD_IDLE_TIMEOUT 3000
+#define CAPS_WORD_IDLE_TIMEOUT 2000
+#define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 #define USB_POLLING_INTERVAL_MS 1
 
 // Tap-hold settings
