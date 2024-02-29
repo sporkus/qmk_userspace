e2454b4620935f5cdf635b72ce00a16ca9f321f5
spork <feedthefoon@gmail.com>
2024-02-28 19:55:04 -0800

    try again

--- keyboards/sporkus/le_chiffre_32/mcuconf.h
+++ keyboards/sporkus/le_chiffre_32/mcuconf.h
@@ -1,2 +1,13 @@
+#pragma once
+
+#include_next <mcuconf.h>
+
 #undef STM32_I2C_USE_I2C2
 #define STM32_I2C_USE_I2C2 TRUE
+
+#undef STM32_I2C_USE_DMA
+#define STM32_I2C_USE_DMA FALSE
+
+#undef STM32_PWM_USE_TIM3
+#define STM32_PWM_USE_TIM3 TRUE
+
