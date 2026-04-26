dc744ae85669e6c448d5f12f8fe70ee933d2aa6d
Foon Wong <feedthefoon@gmail.com>
2026-04-26 13:41:49 -0700

    bully edit from a long time ago

--- keyboards/mkh_studio/bully/keymaps/sporkus/keymap.c
+++ keyboards/mkh_studio/bully/keymaps/sporkus/keymap.c
@@ -9,9 +9,9 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_w(
           KC_TAB,              LEFT_ROW1(_BASE),        RIGHT_ROW1(_BASE),        KC_DEL,  KC_BSPC, \
-          NAV_ESC,             HRML(LEFT_ROW2(_BASE)),  HRMR(RIGHT_ROW2(_BASE)),  KC_COLN, \
+          NAV_ESC,             HRML(LEFT_ROW2(_BASE)),  HRMR(RIGHT_ROW2(_BASE)),  KC_SCLN, \
           KC_LSFT,             LEFT_ROW3(_BASE),        RIGHT_ROW3(_BASE),        SFT_ENT, \
-KC_LCTL,  KC_LALT,  KC_LGUI,   NUM_SPC,         XXX,    NAV_SPC,   KC_RGUI, KC_RALT, KC_RCTL
+KC_LCTL,  KC_LALT,  KC_LGUI,   NUM_SPC,         NAV_SPC,    NAV_SPC,   KC_RGUI, KC_RALT, KC_RCTL
     ),
     [1] = LAYOUT_w(
            _______,             LEFT_ROW1(_NUM),        RIGHT_ROW1(_NUM),   KC_LBRC, KC_RBRC, \
